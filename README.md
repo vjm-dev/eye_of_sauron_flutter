ceb5e0016b58e9c1f49952af6049135779b1edba
vjm-dev <vjaume@outlook.com>
2025-08-08 10:42:22 +0200

    1

---
+++ README.md
@@ -0,0 +1,6 @@
+# eye_of_sauron_flutter
+
+Eye of Sauron particle effects.
+
+The eye blinks every *x* seconds around a container that collapses it.
+
---
+++ android/app/src/main/kotlin/com/example/eye_of_sauron_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.eye_of_sauron_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
